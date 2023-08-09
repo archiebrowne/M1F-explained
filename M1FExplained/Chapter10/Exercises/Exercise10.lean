b0f31c3e53d9d7f0acc0c4e485fb4b85f3f42ee1
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-09 18:28:45 +0100

    decide to use induction on EX10 part a

--- M1FExplained/Chapter10/Exercises/Exercise10.lean
+++ M1FExplained/Chapter10/Exercises/Exercise10.lean
@@ -15,6 +15,24 @@ one containing 4 O’Nuggets, and the other containing 9 O’Nuggets.
 Prove that for any integer n > 23, it is possible for Ivor to buy n O’Nuggets
 (assuming he has enough money).
 -/
+#check induction
+lemma part_a (n : ℕ) (hn : 23 < n) : ∃ (a b : ℕ), 4 * a + 9 * b = n := by
+  induction (n - 24) with
+  sorry
+
+  /-
+  Do induction on n. Prove n = 24 trivially
+  -- assume ∃ aₖ, bₖ, 9aₖ + 4bₖ = k
+  -- contidion on bₖ = 0, 1, else
+  -- bₖ = 0:
+    -- aₖ₊₁ = aₖ - 3, bₖ₊₁ = 7 (since aₖ ≥ 3)
+  -- bₖ = 1:
+    -- aₖ₊₁ = aₖ - 3, bₖ₊₁ = 8 (since aₖ ≥ 3)
+  -- bₖ ≥ 2:
+    -- aₖ₊₁ = aₖ bₖ₊₁ = bₖ - 2 (since bₖ ≥ 2)
+  
+  -/
+sorry
 
 -- part b
 
