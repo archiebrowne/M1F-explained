d3fb5fc26e026da51c980737c0521089fdd8c80e
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-09 17:28:54 +0100

    State problem

--- M1FExplained/Chapter10/Exercises/Exercise06.lean
+++ M1FExplained/Chapter10/Exercises/Exercise06.lean
@@ -2,4 +2,18 @@ import Mathlib.Tactic
 
 namespace Chapter10.Exercise06
 
+-- part a
+
+/-
+Let a, b, c ∈ ℤ. Define the reatest common divisor gcd(a, b, c) to 
+be the largest positive integer that divides a, b, c. Prove that 
+there are integers s, t, u such that gcd(a, b, c) = s * a + t * b + u * c.
+-/
+
+-- part b
+
+/-
+Find such integers s, t, u when a = 91, b = 903, c = 1792.
+-/
+
 end Chapter10.Exercise06
