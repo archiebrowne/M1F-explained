c75cafee276aec456ce2688c47820cd8b8aa2772
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-09 17:32:14 +0100

    state problem EX10

--- M1FExplained/Chapter10/Exercises/Exercise10.lean
+++ M1FExplained/Chapter10/Exercises/Exercise10.lean
@@ -2,6 +2,34 @@ import Mathlib.Tactic
 
 namespace Chapter10.Exercise10
 
+/-
+After a particularly exciting viewing of the new Danish thriller Den heletal, critic Ivor Smallbrain 
+repairs for refreshment to the prison’s highsecurity fast-food outlet O’Ducks. He decides that he’d 
+like to eat some delicious Chicken O’Nuggets. These are sold in packs of two sizes —
+one containing 4 O’Nuggets, and the other containing 9 O’Nuggets.
+-/
 
+-- part a
+
+/-
+Prove that for any integer n > 23, it is possible for Ivor to buy n O’Nuggets
+(assuming he has enough money).
+-/
+
+-- part b
+
+/-
+Perversely, however, Ivor decides that he must buy exactly 23 O’Nuggets,
+no more and no less. Is he able to do this?
+-/
+
+-- part c
+
+/-
+Generalize this question, replacing 4 and 9 by any pair a,b of coprime
+positive integers: find an integer N (depending on a and b), such that
+for any integer n > N it is possible to find integers s,t ≥ 0 satisfying
+s * a + t * b = n, but no such s,t exist satisfying sa+tb = N.
+-/
 
 end Chapter10.Exercise10
